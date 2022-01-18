875ea6514ebb6169909f618b9d79d361623bca1d
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-18 22:32:22 +0200

    scss

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -2,20 +2,27 @@ const BASE_URL = 'https://pixabay.com/api/';
 const apiKey = '25300151-6154e4a76e8e82454cce4100c';
 
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
 export default async function fetchImages(query, page = 1, perPage = 40) {
-  console.log('page=', page);
   if (!query || !query.trim() || page > Math.floor(500 / perPage) + 1) {
     return Promise.resolve('');
   }
-  return await fetch(
-    `${BASE_URL}?image_type=photo&orientation=horizontal&safeserach=true&page=${page}&per_page=${perPage}&key=${apiKey}&q=${query}`,
-  )
+  return await axios
+    .get(
+      `${BASE_URL}?image_type=photo&orientation=horizontal&safeserach=true&page=${page}&per_page=${perPage}&key=${apiKey}&q=${query}`,
+    )
     .then(response => {
-      if (!response.ok) {
+      console.log('total', Number(response.data.total));
+      if (Number(response.status) !== 200) {
         throw new Error(Notiflix.Notify.failure(`Error ${response.status}`));
       }
-      return response.json();
+      if (Number(response.data.total) !== 0) {
+        return response.data;
+      }
+      Notiflix.Notify.failure(
+        '❌ Sorry, there are no images matching your search query. Please try again.',
+      );
     })
     .catch(() =>
       Notiflix.Notify.failure(
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import fetchImages from './fetchImages';
 import photoCard from './templates/photoCard.hbs';
+import Notiflix from 'notiflix';
 
 const refs = {
   formSeachEl: document.querySelector('.search-form'),
@@ -9,18 +10,22 @@ const refs = {
 let page = 1;
 const perPage = 40;
 let query = '';
+let totalHits = 500;
 refs.formSeachEl.addEventListener('submit', getImages);
 
 function getImages(e) {
   e.preventDefault();
 
   page = 1;
+
   refs.galleryEl.innerHTML = '';
   query = e.target.searchQuery.value;
   fetchImages(query, page, perPage).then(createMarkup);
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images`);
 }
 
 function createMarkup(dataImages) {
+  console.log(dataImages.hits);
   const markup = photoCard(dataImages.hits);
   refs.galleryEl.innerHTML += markup;
   renderOnIntersectionObserverApi(dataImages.hits);
