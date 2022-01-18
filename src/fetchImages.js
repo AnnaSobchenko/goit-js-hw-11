07bcf8a7e0f0994c312314e1e49687256fd4932a
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-18 17:31:44 +0200

    gallery

---
+++ src/fetchImages.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const apiKey = '25300151-6154e4a76e8e82454cce4100c';
+
+import Notiflix from 'notiflix';
+
+export default async function fetchImages(query, page = 1, perPage = 40) {
+  if (!query || !query.trim()) {
+    return Promise.resolve('');
+  }
+  return await fetch(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&safeserach=true&page=${page}&per_page=${perPage}&key=${apiKey}&q=${query}`,
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(Notiflix.Notify.failure(`Error ${response.status}`));
+      }
+      return response.json();
+    })
+    .catch(() =>
+      Notiflix.Notify.failure(
+        '❌ Sorry, there are no images matching your search query. Please try again.',
+      ),
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+import fetchImages from './fetchImages';
+import photoCard from './templates/photoCard.hbs';
+
+const refs = {
+  formSeachEl: document.querySelector('.search-form'),
+  galleryEl: document.querySelector('.gallery'),
+};
+
+refs.formSeachEl.addEventListener('submit', getImages);
+
+function getImages(e) {
+  e.preventDefault();
+  // console.log(e.target.searchQuery.value);
+
+  fetchImages(e.target.searchQuery.value).then(createMarkup);
+}
+
+function createMarkup(dataImages) {
+  const markup = photoCard(dataImages.hits);
+  refs.galleryEl.innerHTML = markup;
+  console.log(markup);
+}
